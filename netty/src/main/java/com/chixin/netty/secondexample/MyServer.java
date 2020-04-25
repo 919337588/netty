34dc7c038acb01fc586200c0b92ab13163f7b5b0
chixin <chixin@zk01.cn>
2020-04-25 10:00:08 +0800

    first

--- netty/src/main/java/com/chixin/netty/secondexample/MyServer.java
+++ netty/src/main/java/com/chixin/netty/secondexample/MyServer.java
@@ -1,4 +1,25 @@
 package com.chixin.netty.secondexample;
 
+import com.chixin.netty.firstexample.TestServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class MyServer {
+    public static void main(String[] args)  throws  Exception{
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+
+            ServerBootstrap serverBootstrap=new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new MyServerInitializer());
+            ChannelFuture sync = serverBootstrap.bind(8899).sync();
+            sync.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
 }
